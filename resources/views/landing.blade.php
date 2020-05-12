71e1b6e9c5ff64c83bebf6405f16655fbe0405d9
Andika Tedja <andikatedja@yahoo.com>
2020-05-12 12:55:33 +0800

    Add image to landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -58,7 +58,7 @@
     </nav>
 
     <!-- Header -->
-    <header class="bg-primary py-5">
+    <header class="bg-blue py-5">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-lg-6">
@@ -66,8 +66,7 @@
                     <p class="lead mb-5 text-white-50">@lang('landing.tagline')</p>
                 </div>
                 <div class="col-lg-6">
-                    <img class="img-fluid d-none d-lg-block" src="https://dummyimage.com/600x400/000/fff" alt=""
-                        srcset="">
+                    <img class="img-fluid d-none d-lg-block" src="{{asset('img/landing/header.png')}}" alt="" srcset="">
                 </div>
             </div>
         </div>
@@ -78,54 +77,47 @@
     </section>
 
     <!-- Page Content -->
-    <section class="kelebihan bg-primary text-white">
+    <section class="kelebihan bg-blue text-white">
         <div class="container p-5">
             <div class="row">
                 <div class="col-lg-6">
-                    <h4>Kelebihan 1</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nisi dolores quam id laboriosam
-                        totam
-                        laudantium soluta eos recusandae exercitationem consequuntur doloribus, sed deleniti sint qui ut
-                        accusamus aliquid libero.</p>
+                    <h4>Peralatan Lengkap dan Canggih</h4>
+                    <p>Laundry kami menggunakan peralatan yang cukup lengkap dan canggih. Peralatan kami memungkinkan
+                        baju tidak perlu dijemur dan mengurangi debu pada baju</p>
                 </div>
                 <div class="col-lg-6">
-                    <img class="img-fluid d-none d-lg-block" src="https://dummyimage.com/600x200/000/fff" alt=""
-                        srcset="">
+                    <img class="img-fluid d-none d-lg-block" src="{{asset('img/landing/alat.png')}}" alt="" srcset="">
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="kelebihan bg-primary text-white">
+    <section class="kelebihan bg-blue text-white">
         <div class="container p-5">
             <div class="row">
                 <div class="col-lg-6">
-                    <img class="img-fluid d-none d-lg-block" src="https://dummyimage.com/600x200/000/fff" alt=""
+                    <img class="img-fluid d-none d-lg-block" src="{{asset('img/landing/tipebaju.png')}}" alt=""
                         srcset="">
                 </div>
                 <div class="col-lg-6">
-                    <h4>Kelebihan 2</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nisi dolores quam id laboriosam
-                        totam
-                        laudantium soluta eos recusandae exercitationem consequuntur doloribus, sed deleniti sint qui ut
-                        accusamus aliquid libero.</p>
+                    <h4>Segala Tipe Pakaian</h4>
+                    <p>Laundry kami menerima segala tipe pakaian mulai dari baju, celana, jas, gorden, bed cover,
+                        selimut, seprei, karpet, dan lain lain.</p>
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="kelebihan bg-primary text-white">
+    <section class="kelebihan bg-blue text-white">
         <div class="container p-5">
             <div class="row">
                 <div class="col-lg-6">
-                    <h4>Kelebihan 3</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nisi dolores quam id laboriosam
-                        totam
-                        laudantium soluta eos recusandae exercitationem consequuntur doloribus, sed deleniti sint qui ut
-                        accusamus aliquid libero.</p>
+                    <h4>Pegawai Profesional</h4>
+                    <p>Laundry kami terdiri dari pegawai-pegawai yang profesional yang mampu bekerja dalam tim dengan
+                        cukup baik dan handal di bidangnya sehingga membuat laundry kami minim kesalahan</p>
                 </div>
                 <div class="col-lg-6">
-                    <img class="img-fluid d-none d-lg-block" src="https://dummyimage.com/600x200/000/fff" alt=""
+                    <img class="img-fluid d-none d-lg-block" src="{{asset('img/landing/pegawai.png')}}" alt=""
                         srcset="">
                 </div>
             </div>
@@ -136,37 +128,37 @@
         <h3>Apa saja yang bisa kami laundry?</h3>
     </section>
 
-    <section class="bg-primary p-5 text-center">
+    <section class="bg-blue p-5 text-center">
         <div class="container">
             <div class="row flex-row flex-nowrap kategori">
                 <div class="col-4 mb-2">
                     <div class="card">
-                        <img src="https://dummyimage.com/400x300/000/fff" class="card-img-top" alt="">
-                        <div class="card-body">
+                        <img src="{{asset('img/landing/Baju.png')}}" class="card-img-top" alt="">
+                        <div class="card-body d-none d-lg-block">
                             <p class="card-text">Baju</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-4 mb-2">
                     <div class="card">
-                        <img src="https://dummyimage.com/400x300/000/fff" class="card-img-top" alt="">
-                        <div class="card-body">
+                        <img src="{{asset('img/landing/Celana.jpg')}}" class="card-img-top" alt="">
+                        <div class="card-body d-none d-lg-block">
                             <p class="card-text">Celana</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-4 mb-2">
                     <div class="card">
-                        <img src="https://dummyimage.com/400x300/000/fff" class="card-img-top" alt="">
-                        <div class="card-body">
-                            <p class="card-text">Bed Cover</p>
+                        <img src="{{asset('img/landing/Jas.jpg')}}" class="card-img-top" alt="">
+                        <div class="card-body d-none d-lg-block">
+                            <p class="card-text">Jas</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-4 mb-2">
                     <div class="card">
                         <img src="https://dummyimage.com/400x300/000/fff" class="card-img-top" alt="">
-                        <div class="card-body">
+                        <div class="card-body d-none d-lg-block">
                             <p class="card-text">Selimut</p>
                         </div>
                     </div>
@@ -179,16 +171,23 @@
         <h3>Temukan kami!</h3>
     </section>
 
-    <section class="text-white bg-primary">
+    <section class="text-white bg-blue">
         <div class="container p-5">
             <div class="row">
                 <div class="col-md-6 mb-4 mb-sm-0">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed iste architecto suscipit voluptatum,
-                    consectetur fuga fugiat possimus quasi, iure enim esse, molestias necessitatibus sint quas nihil!
-                    Placeat iure dolorum reiciendis?
+                    <h5>Alamat</h5>
+                    <p>Jalan Raya Sempidi No. 69 Lukluk, Mengwi, Badung, Bali</p>
+                    <br>
+                    <h5>Kontak</h5>
+                    <p>laundryxyz@gmail.com</p>
+                    <p>(0361)123456</p>
+                    <p>081234567890</p>
                 </div>
                 <div class="col-md-6">
-                    <img class="img-fluid" src="https://dummyimage.com/600x300/000/fff" alt="" srcset="">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.0551329606237!2d115.18456241433559!3d-8.59069798958535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2394d265755b3%3A0xb025730a3074e799!2sJl.%20Raya%20Lukluk%20-%20Sempidi%2C%20Lukluk%2C%20Kec.%20Mengwi%2C%20Kabupaten%20Badung%2C%20Bali!5e0!3m2!1sid!2sid!4v1589257661013!5m2!1sid!2sid"
+                        width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""
+                        aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
         </div>
