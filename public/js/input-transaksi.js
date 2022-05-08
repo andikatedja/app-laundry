8aac886bc9d1db55e7d2242d43372a0cf11f3254
Andika Tedja <andikatedja@yahoo.com>
2022-05-08 10:53:32 +0800

    Update parseInt

--- public/js/input-transaksi.js
+++ public/js/input-transaksi.js
@@ -1,36 +1,36 @@
-let fixTotal = $('#total-harga').val();
-let total = $('#total-harga').val();
+let fixTotal = $("#total-harga").val();
+let total = $("#total-harga").val();
 
-$('#tambah-transaksi').on('click', function () {
-    console.log('ok');
+$("#tambah-transaksi").on("click", function () {
+    console.log("ok");
 });
 
-$('#bayarModal').on('shown.bs.modal', function () {
-    $(this).find('#input-bayar').focus();
+$("#bayarModal").on("shown.bs.modal", function () {
+    $(this).find("#input-bayar").focus();
 });
 
-$('#input-bayar').on('keyup', function () {
-    $('#kembalian').html($('#input-bayar').val() - total);
+$("#input-bayar").on("keyup", function () {
+    $("#kembalian").html($("#input-bayar").val() - total);
 });
 
-$('#btn-simpan').on('click', function (event) {
-    if ($('#input-bayar').val() < total) {
+$("#btn-simpan").on("click", function (event) {
+    if (parseInt($("#input-bayar").val()) < parseInt(total)) {
         event.preventDefault();
-        alert('Pembayaran kurang!');
+        alert("Pembayaran kurang!");
     }
 });
 
-$('#voucher').on('change', function () {
-    let potongan = $('option:selected', this).data('potong');
+$("#voucher").on("change", function () {
+    let potongan = $("option:selected", this).data("potong");
     if (potongan != 0) {
         total = fixTotal;
         total -= potongan;
-        total < 0 ? total = 0 : total;
-        $('#total-harga').val(total);
-        $('#kembalian').html($('#input-bayar').val() - total);
+        total < 0 ? (total = 0) : total;
+        $("#total-harga").val(total);
+        $("#kembalian").html($("#input-bayar").val() - total);
     } else {
         total = fixTotal;
-        $('#total-harga').val(total);
-        $('#kembalian').html($('#input-bayar').val() - total);
+        $("#total-harga").val(total);
+        $("#kembalian").html($("#input-bayar").val() - total);
     }
 });
