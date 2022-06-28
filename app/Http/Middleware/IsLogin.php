466a6884dc15cd2301a5a728b747da5dcc7d90d2
Andika Tedja <andikatedja@yahoo.com>
2022-06-28 14:58:14 +0800

    Update login middleware

--- app/Http/Middleware/IsLogin.php
+++ app/Http/Middleware/IsLogin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Lang;
 
@@ -17,25 +18,22 @@ class IsLogin
      */
     public function handle($request, Closure $next)
     {
-        if ($request->hasCookie('login') && $request->hasCookie('login_key')) {
-            $email = $request->cookie('login'); //Email
-            $secretKey = $request->cookie('login_key');
+        // if ($request->hasCookie('login') && $request->hasCookie('login_key')) {
+        //     $email = $request->cookie('login'); //Email
+        //     $secretKey = $request->cookie('login_key');
 
-            if ($secretKey === hash('sha256', env('COOKIE_SECRET_KEY', 'DefaultKey'))) {
-                $request->session()->put('login', $email);
-            }
-        }
+        //     if ($secretKey === hash('sha256', env('COOKIE_SECRET_KEY', 'DefaultKey'))) {
+        //         $request->session()->put('login', $email);
+        //     }
+        // }
 
         switch ($request->segment(1)) {
             case 'login':
-                if (!$request->session()->has('login')) {
+                if (!Auth::check()) {
                     return $next($request);
                 }
 
-                $email_login = $request->session()->get('login');
-                $user_login = DB::table('users')->where('email', '=', $email_login)->first();
-
-                if ($user_login->role == '1') {
+                if (Auth::user()->role == '1') {
                     return redirect('admin');
                 } else {
                     return redirect('member');
@@ -43,14 +41,14 @@ class IsLogin
                 break;
 
             case 'admin':
-                if (!$request->session()->has('login')) {
+                if (!Auth::check()) {
                     return redirect('login')->with('error', Lang::get('auth.please_login'));
                 }
 
-                $userSession = $request->session()->get('login');
-                $user = DB::table('users')->where('email', '=', $userSession)->first();
+                // $userSession = $request->session()->get('login');
+                // $user = DB::table('users')->where('email', '=', $userSession)->first();
 
-                if ($user->role == '1') {
+                if (Auth::user()->role == '1') {
                     return $next($request);
                 } else {
                     return redirect('member');
@@ -58,14 +56,14 @@ class IsLogin
                 break;
 
             case 'member':
-                if (!$request->session()->has('login')) {
+                if (!Auth::check()) {
                     return redirect('login')->with('error', Lang::get('auth.please_login'));
                 }
 
-                $userSession = $request->session()->get('login');
-                $user = DB::table('users')->where('email', '=', $userSession)->first();
+                // $userSession = $request->session()->get('login');
+                // $user = DB::table('users')->where('email', '=', $userSession)->first();
 
-                if ($user->role == '2') {
+                if (Auth::user()->role == '2') {
                     return $next($request);
                 } else {
                     return redirect('admin');
@@ -76,5 +74,56 @@ class IsLogin
                 return $next($request);
                 break;
         }
+
+        // switch ($request->segment(1)) {
+        //     case 'login':
+        //         if (!$request->session()->has('login')) {
+        //             return $next($request);
+        //         }
+
+        //         $email_login = $request->session()->get('login');
+        //         $user_login = DB::table('users')->where('email', '=', $email_login)->first();
+
+        //         if ($user_login->role == '1') {
+        //             return redirect('admin');
+        //         } else {
+        //             return redirect('member');
+        //         }
+        //         break;
+
+        //     case 'admin':
+        //         if (!$request->session()->has('login')) {
+        //             return redirect('login')->with('error', Lang::get('auth.please_login'));
+        //         }
+
+        //         $userSession = $request->session()->get('login');
+        //         $user = DB::table('users')->where('email', '=', $userSession)->first();
+
+        //         if ($user->role == '1') {
+        //             return $next($request);
+        //         } else {
+        //             return redirect('member');
+        //         }
+        //         break;
+
+        //     case 'member':
+        //         if (!$request->session()->has('login')) {
+        //             return redirect('login')->with('error', Lang::get('auth.please_login'));
+        //         }
+
+        //         $userSession = $request->session()->get('login');
+        //         $user = DB::table('users')->where('email', '=', $userSession)->first();
+
+        //         if ($user->role == '2') {
+        //             return $next($request);
+        //         } else {
+        //             return redirect('admin');
+        //         }
+        //         break;
+
+        //     default:
+        //         return $next($request);
+        //         break;
+        // }
     }
 }
