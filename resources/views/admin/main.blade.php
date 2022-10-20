f022d102f3284cff49bd856808fcaddffba9dee6
andikatedja <andikatedja@yahoo.com>
2022-10-20 11:51:24 +0800

    Update menu urls

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -27,8 +27,9 @@
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
-                            class="fas fa-bars"></i></a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+                        <i class="fas fa-bars"></i>
+                    </a>
                 </li>
             </ul>
 
@@ -71,63 +72,64 @@
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <li class="nav-item">
-                            <a href="{{ url('admin') }}" class="nav-link {{ request()->is('admin') ? 'active' : '' }}">
+                            <a href="{{ route('admin.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/input-transaksi') }}"
-                                class="nav-link {{ request()->is('admin/input-transaksi') ? 'active' : '' }}">
+                            <a href="{{ route('admin.transactions.create') }}"
+                                class="nav-link {{ request()->routeIs('admin.transactions.create') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-file-invoice"></i>
                                 <p>Input Transaksi</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/transaksi') }}"
-                                class="nav-link {{ request()->is('admin/transaksi*') ? 'active' : '' }}">
+                            <a href="{{ route('admin.transactions.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.transactions.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-history"></i>
                                 <p>Riwayat Transaksi</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/harga') }}"
-                                class="nav-link {{ request()->is('admin/harga') ? 'active' : '' }}">
+                            <a href="{{ route('admin.price-lists.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.price-lists.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-list"></i>
                                 <p>Daftar Harga</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/members') }}"
-                                class="nav-link {{ request()->is('admin/members*') ? 'active' : '' }}">
+                            <a href="{{ route('admin.members.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.members*') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-users"></i>
                                 <p>Daftar Member</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/voucher') }}"
-                                class="nav-link {{ request()->is('admin/voucher') ? 'active' : '' }}">
+                            <a href="{{ route('admin.vouchers.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.vouchers.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-star"></i>
                                 <p>Voucher</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/saran') }}"
-                                class="nav-link {{ request()->is('admin/saran') ? 'active' : '' }}">
+                            <a href="{{ route('admin.complaint-suggestions.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.complaint-suggestions.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-sticky-note"></i>
                                 <p>Saran / Komplain</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('admin/laporan') }}"
-                                class="nav-link {{ request()->is('admin/laporan') ? 'active' : '' }}">
+                            <a href="{{ route('admin.reports.index') }}"
+                                class="nav-link {{ request()->routeIs('admin.reports.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-file-alt"></i>
                                 <p>Laporan Keuangan</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="{{ route('profile.index') }}"
-                                class="nav-link {{ request()->routeIs('profile.show') ? 'active' : '' }}">
+                                class="nav-link {{ request()->routeIs('profile.index') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-user-edit"></i>
                                 <p>Edit Profil</p>
                             </a>
